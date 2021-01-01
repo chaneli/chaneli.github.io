c975b3d6a13aef2025d54690d75573415e65813a
chaneli <chaneli@student.chalmers.se>
2021-01-01 18:23:47 +0100

    Main view created with Vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="app">
+  <h1>This view is created with Vue</h1>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
